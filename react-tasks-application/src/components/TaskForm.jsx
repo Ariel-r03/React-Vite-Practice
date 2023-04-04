dcce3a5c873e22310a323649c4c39fc731a6f4ce
Ariel-r03 <arieladolfo.silva@gmail.com>
2023-04-04 12:22:21 -0600

    Last updates

--- react-tasks-application/src/components/TaskForm.jsx
+++ react-tasks-application/src/components/TaskForm.jsx
@@ -4,29 +4,33 @@ import { TaskContext } from "../context/TaskContext";
 function TaskForm() {
   const [Title, setTitle] = useState(null);
   const [Description, setDescription] = useState(null);
-  const value = useContext(TaskContext);
-  console.log(value);
+  const {createTask} = useContext(TaskContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    value.createTask({ title: Title, description: Description });
+    createTask({ title: Title, description: Description });
     e.target.reset();
   };
   return (
-    <form onSubmit={handleSubmit}>
+   <div className="max-w-md mx-auto">
+     <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4">
+      <h1 className="text-2xl font-bold text-white mb-3">Crea tu tarea</h1>
       <input
         onChange={(e) => {
           setTitle(e.target.value);
         }}
+        className="bg-slate-300 p-3 w-full mb-2"
       ></input>
 
       <textarea
         onChange={(e) => {
           setDescription(e.target.value);
         }}
+        className="bg-slate-300 p-3 w-full mb-2"
       ></textarea>
-      <button type="submit">Guardar</button>
+      <button className="bg-indigo-500 px-3 py-1 text-white" type="submit">Guardar</button>
     </form>
+    </div>
   );
 }
 
